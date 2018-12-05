b12e6857f71fc9f8686069bb817a8eee98c934a4
MokotoYutakana <Pusana@hotmail.co.th>
2018-12-05 15:40:27 +0700

    Added background music. (I can't finad LevelPane)

--- src/application/Board.java
+++ src/application/Board.java
@@ -13,11 +13,13 @@ import javafx.scene.shape.Path;
 import logic.Cookie;
 
 public class Board extends Pane {
+	private static String bgmPath;
 	
 	Random ran = new Random();
 	public Board() {
 		setPadding(new Insets(15));
 		setPrefWidth(850);
+		loadPath();
 	}
 	
 	public void addCookie(Console console,LevelPane lp) {
@@ -25,10 +27,14 @@ public class Board extends Pane {
 		cookie.setUpCookie(console,lp);
 		getChildren().addAll(cookie);
     }
+	
+	public static void loadPath() {
+		bgmPath = "res/Fluffing a Duck.mp3";
+	}
     
     // Plays background music (Still not add music)
     public void startBgmLoop() {
-		AudioClip bgm = new AudioClip("bgmpath");
+		AudioClip bgm = new AudioClip(new File(bgmPath).toURI().toString());
 		bgm.play();
     }
     public void reset() {
